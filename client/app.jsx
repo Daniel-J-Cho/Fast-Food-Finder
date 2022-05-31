1f136692f9859d49eeac97e38eb558e240a2e62e
Daniel Cho <danielcho07@gmail.com>
2022-05-31 06:45:45 +0000

    Hash-routing in-progess. #1

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import Home from './pages/home';
+import { parseRoute } from './lib';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      this.setState({ route: parseRoute(window.location.hash) });
+    });
+  }
+
   render() {
-    return <Home />;
+    return (
+    <div className="container">
+      <h1 className="text-center">Fast Food Finder</h1>
+        <a href="#" className="btn btn-primary"><i className="fa fa-home"></i></a>
+      <Home />
+    </div>
+    );
   }
 }
