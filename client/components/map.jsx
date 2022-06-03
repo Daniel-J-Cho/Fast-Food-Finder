19ad0ae7f36cb805661723b3f6bafc6da8355027
Daniel Cho <danielcho07@gmail.com>
2022-06-03 00:39:02 +0000

    Added user location functionality. #2.

--- client/components/map.jsx
+++ client/components/map.jsx
@@ -1,19 +1,60 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React from 'react';
+class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { coords: null };
+    this.map = null;
+    this.marker = null;
+    this.mapDivRef = React.createRef();
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-const Map = () => {
-  const ref = useRef(null);
-  const [map, setMap] = useState();
+  componentDidMount() {
+    if (this.mapDivRef.current && !this.map) {
+      this.map = new window.google.maps.Map(this.mapDivRef.current);
 
-  useEffect(() => {
-    if (ref.current && !map) {
-      setMap(new window.google.maps.Map(ref.current, {}));
     }
-    if (map) {
-      map.setOptions({ zoom: 11, center: { lat: 33.669445, lng: -117.823059 } });
+    if (this.map) {
+      this.map.setOptions({ zoom: 11, center: { lat: 33.669445, lng: -117.823059 } });
     }
-  }, [ref, map]);
+  }
 
-  return <div ref={ref} style={{ height: '68vh', width: '81vw', margin: 'auto' }}/>;
-};
+  handleClick() {
+    navigator.geolocation.getCurrentPosition(position => {
+      if (position.coords) {
+        this.setState({ coords: position.coords });
+        this.map.setOptions({ center: { lat: position.coords.latitude, lng: position.coords.longitude } });
+        this.marker = new window.google.maps.Marker();
+        this.marker.setPosition({ lat: position.coords.latitude, lng: position.coords.longitude });
+        this.marker.setMap(this.map);
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="modal" id="permModal" tabIndex="-1" aria-labelledby="permModalLabel" aria-hidden="true">
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="permModalLabel">Enable Location Services</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Cancel"></button>
+              </div>
+              <div className="modal-body">
+                <p>Are you sure you want to enable location services?</p>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-light" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={this.handleClick}>Confirm</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div ref={this.mapDivRef} style={{ height: '68vh', width: '81vw', margin: 'auto' }} />
+      </div>
+    );
+  }
+}
 
 export default Map;
