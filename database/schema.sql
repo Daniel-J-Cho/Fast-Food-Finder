643e1e771ef9e07313c5f8d4f8e0cbe19b869aac
Daniel Cho <danielcho07@gmail.com>
2022-06-03 20:12:49 +0000

    Setting up database in-progess. #2

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,47 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+ CREATE TABLE "public.locations" (
+	"locationId" serial NOT NULL,
+	"restaurantName" TEXT NOT NULL,
+	"lat" float8 NOT NULL,
+	"lng" float8 NOT NULL,
+	"address" TEXT NOT NULL,
+	"googlePlaceId" TEXT NOT NULL,
+	CONSTRAINT "locations_pk" PRIMARY KEY ("locationId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.comments" (
+	"commentId" serial NOT NULL,
+	"comment" TEXT NOT NULL,
+	"entryId" integer NOT NULL,
+	"createdAt" timestamptz NOT NULL,
+	CONSTRAINT "comments_pk" PRIMARY KEY ("commentId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.favoriteEntries" (
+	"entryId" serial NOT NULL,
+	"locationId" integer NOT NULL,
+	"comment" TEXT NOT NULL,
+	"logoUrl" TEXT NOT NULL,
+	"createdAt" timestamptz NOT NULL,
+	CONSTRAINT "favoriteEntries_pk" PRIMARY KEY ("entryId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "comments" ADD CONSTRAINT "comments_fk0" FOREIGN KEY ("entryId") REFERENCES "favoriteEntries"("entryId");
+
+ALTER TABLE "favoriteEntries" ADD CONSTRAINT "favoriteEntries_fk0" FOREIGN KEY ("locationId") REFERENCES "locations"("locationId");
