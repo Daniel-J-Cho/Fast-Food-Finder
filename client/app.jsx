2202244c936b40b1353ca85f4adcd75e52c80489
Daniel Cho <danielcho07@gmail.com>
2022-06-13 00:01:25 +0000

    Fixed hash routing and added 'favorites-view' page. #6

--- client/app.jsx
+++ client/app.jsx
@@ -6,6 +6,7 @@ import LocationMarker from './components/location-marker';
 import { parseRoute } from './lib';
 import { Wrapper } from '@googlemaps/react-wrapper';
 import Map from './components/map';
+import FavoritesView from './pages/favorites-view';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -21,33 +22,71 @@ export default class App extends React.Component {
     });
   }
 
-  render() {
+  renderPage() {
+    const { route } = this.state;
+
     const render = Status => {
       return <h1>{Status}</h1>;
     };
 
+    if (route.path === '') {
+      return (
+        <div className="container">
+          <div className="row">
+            <div className="home-button d-flex align-items-center">
+              <HomeButton />
+            </div>
+            <div className="col-1 favorites-button d-flex align-items-center">
+              <FavoritesButton />
+            </div>
+            <div className="col d-flex justify-content-end">
+              <LocationMarker />
+            </div>
+          </div>
+          <div className="row mt-lg-3">
+            <Wrapper apiKey={process.env.GOOGLE_MAPS_API_KEY} render={render} libraries={['places']} >
+              <Map />
+            </Wrapper>
+          </div>
+        </div>
+      );
+    }
+    if (route.path === 'favorites') {
+      return <FavoritesView />;
+    }
+  }
+
+  render() {
+    // const render = Status => {
+    //   return <h1>{Status}</h1>;
+    // };
+
     return (
-    <div className="container">
-      <div className="row">
-        <MainHeader />
-      </div>
-      <div className="row">
-        <div className="home-button d-flex align-items-center">
-          <HomeButton />
-        </div>
-        <div className="col-1 favorites-button d-flex align-items-center">
-          <FavoritesButton />
-        </div>
-        <div className="col d-flex justify-content-end">
-          <LocationMarker />
-        </div>
-      </div>
-      <div className="row mt-lg-3">
-        <Wrapper apiKey={process.env.GOOGLE_MAPS_API_KEY} render={render} libraries={['places']} >
-          <Map />
-        </Wrapper>
-      </div>
-    </div>
+    // <div className="container">
+    <>
+      <MainHeader />
+      { this.renderPage() }
+    </>
+    // <div className="row">
+    //   <MainHeader />
+    // </div>
+    //   {/* <div className="row">
+    //     <div className="home-button d-flex align-items-center">
+    //       <HomeButton />
+    //     </div>
+    //     <div className="col-1 favorites-button d-flex align-items-center">
+    //       <FavoritesButton />
+    //     </div>
+    //     <div className="col d-flex justify-content-end">
+    //       <LocationMarker />
+    //     </div>
+    //   </div>
+    //   <div className="row mt-lg-3">
+    //     <Wrapper apiKey={process.env.GOOGLE_MAPS_API_KEY} render={render} libraries={['places']} >
+    //       <Map />
+    //     </Wrapper>
+    //   </div> */}
+    // // </div>
     );
   }
 }
---
+++ client/pages/favorites-view.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import HomeButton from '../components/home-button';
+
+class FavoritesView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      entries: []
+    };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="home-button d-flex align-items-center">
+            <HomeButton />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FavoritesView;
