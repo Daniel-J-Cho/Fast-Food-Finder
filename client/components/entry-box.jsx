75b644a63497783d7e4b6938c1ebd45f2ca65688
Daniel Cho <danielcho07@gmail.com>
2022-06-15 18:07:47 +0000

    Spaced buttons on entry-box evently. #6

--- client/components/entry-box.jsx
+++ client/components/entry-box.jsx
@@ -8,10 +8,14 @@ function EntryBox(props) {
         <h5 className="card-title">Franchise name goes here</h5>
         <p className="card-text">Address goes here</p>
         <p className="card-text">make this a bullet-pointed comment</p>
-        <button type="button" className="btn btn-primary">Add Comment</button>
-        <button type="button" className="btn btn-secondary">Edit Comment</button>
-        <button type="button" className="btn btn-warning">Delete Comment</button>
-        <button type="button" className="btn btn-danger">Delete Location</button>
+        <div className="row">
+          <div className="d-flex justify-content-around">
+            <button type="button" className="btn btn-primary">Add Comment</button>
+            <button type="button" className="btn btn-secondary">Edit Comment</button>
+            <button type="button" className="btn btn-warning">Delete Comment</button>
+            <button type="button" className="btn btn-danger">Delete Location</button>
+          </div>
+        </div>
       </div>
     </div>
   );
