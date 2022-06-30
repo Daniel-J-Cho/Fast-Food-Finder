ce4d643ce9dc46ab5e3784966ddf527fb9f99b2d
Daniel Cho <danielcho07@gmail.com>
2022-06-30 05:29:34 +0000

    Made a modal for the 'Edit Comment' button. #10

--- client/components/favorite.jsx
+++ client/components/favorite.jsx
@@ -8,12 +8,16 @@ class Favorite extends React.Component {
       restAddress: '',
       restName: '',
       addCommentId: null,
-      displayComments: []
+      displayComments: [],
+      editedComment: '',
+      toBeEdited: [],
+      commentId: null
     };
     this.prepSetToDelete = this.prepSetToDelete.bind(this);
     this.setAddCommentIdAddress = this.setAddCommentIdAddress.bind(this);
     this.createComment = this.createComment.bind(this);
     this.handleCommentChange = this.handleCommentChange.bind(this);
+    this.handleCommentEdit = this.handleCommentEdit.bind(this);
     this.addComment = this.addComment.bind(this);
     this.displayComments = this.displayComments.bind(this);
   }
@@ -21,7 +25,7 @@ class Favorite extends React.Component {
   componentDidMount() {
     fetch(`/api/comments/${this.props.propKey}`)
       .then(res => res.ok ? res.json() : Promise.reject(new Error('Something went wrong')))
-      .then(data => this.setState({ displayComments: data }))
+      .then(data => this.setState({ displayComments: data, toBeEdited: data }))
       .catch(err => console.error('error:', err));
   }
 
@@ -45,6 +49,14 @@ class Favorite extends React.Component {
     comment = event.target.value;
     comments[index] = comment;
     this.setState({ comments });
+  }
+
+  handleCommentEdit(event, index) {
+    const editedComments = [...this.state.toBeEdited];
+    let editedCommentAtIndex = { ...editedComments[index] };
+    editedCommentAtIndex = event.target.value;
+    editedComments[index] = editedCommentAtIndex;
+    this.setState({ toBeEdited: editedComments });
   }
 
   addComment(index) {
@@ -86,7 +98,7 @@ class Favorite extends React.Component {
                 <div className="comments">
                   {this.state.comments.map((comment, index) => {
                     return (
-                    <div key={index}><input value={comment} key={index} className="comment-input" onChange={event => this.handleCommentChange(event, index)} /><br></br><br></br>
+                    <div key={index}><input key={index} className="comment-input" onChange={event => this.handleCommentChange(event, index)} /><br></br><br></br>
                     <button type="submit" onClick={() => this.addComment(index)} id="confirmButton" className="btn btn-primary confirm-button" data-bs-dismiss="modal">Confirm</button><br></br><br></br>
                     </div>
                     );
@@ -96,6 +108,32 @@ class Favorite extends React.Component {
               <div className="modal-footer d-flex justify-content-evenly">
                 <button type="button" className="btn btn-light" data-bs-dismiss="modal">Cancel</button>
                 <button type="button" onClick={event => this.createComment(event)} className="btn btn-secondary">Add comment</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="modal fade" id={`editCommentModal-${this.props.propKey}`} tabIndex="-1" aria-labelledby="editCommentModalLabel" aria-hidden="true">
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="editCommentModalLabel">Edit a comment to this {this.state.restName} entry</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Cancel"></button>
+              </div>
+              <div className="modal-body">
+                <p><b>Address:</b> {this.state.restAddress}</p>
+                <div className="comments">
+                  {this.state.toBeEdited.map((comment, index) => {
+                    return (
+                      <div key={index}><input value={this.state.toBeEdited[index].comment} key={index} className="comment-input" onChange={event => this.handleCommentEdit(event, index)} /><br></br><br></br>
+                        <button type="submit" onClick={() => this.editComment(index)} id="confirmButton" className="btn btn-secondary confirm-button" data-bs-dismiss="modal">Confirm Edit</button><br></br><br></br>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="modal-footer d-flex justify-content-evenly">
+                <button type="button" className="btn btn-light" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" className="btn btn-primary" data-bs-dismiss="modal">Done</button>
               </div>
             </div>
           </div>
@@ -114,7 +152,7 @@ class Favorite extends React.Component {
             <div className="row mt-3">
               <div className="d-flex justify-content-around">
                 <button type="button" onClick={() => this.setAddCommentIdAddress(this.props.propKey, this.props.restAddress, this.props.restName)} className="btn btn-primary entry-btn" data-bs-toggle="modal" data-bs-target={`#addCommentModal-${this.props.propKey}`}>Add Comment</button>
-                <button type="button" className="btn btn-secondary entry-btn">Edit Comment</button>
+                <button type="button" onClick={() => this.setAddCommentIdAddress(this.props.propKey, this.props.restAddress, this.props.restName)} className="btn btn-secondary entry-btn" data-bs-toggle="modal" data-bs-target={`#editCommentModal-${this.props.propKey}`}>Edit Comment</button>
                 <button type="button" className="btn btn-warning entry-btn">Delete Comment</button>
                 <button type="button" onClick={() => this.prepSetToDelete(this.props.propKey, this.props.restAddress, this.props.restName)} className="btn btn-danger entry-btn" data-bs-toggle="modal" data-bs-target="#deleteLocationModal">Delete Location</button>
               </div>
